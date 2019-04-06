e2eaac524837d1a4586744356a3ccc55edeec776
Pleum <chacree.som@cpc.ac.th>
2019-04-07 03:46:41 +0700

    :fire::lipstick:::truck::heavy_plus_sign:

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase/app';
+
+// Initialize Firebase
+const config = {
+  apiKey: 'AIzaSyB7tJgACajfoIgNqv_4Rv-GS6Bgbz9Uid8',
+  authDomain: 'bethere-fd383.firebaseapp.com',
+  databaseURL: 'https://bethere-fd383.firebaseio.com',
+  projectId: 'bethere-fd383',
+  storageBucket: 'bethere-fd383.appspot.com',
+  messagingSenderId: '803549483727'
+};
+
+firebase.initializeApp(config);
+
+export default firebase
